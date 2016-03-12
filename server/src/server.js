a802da7af450eaf80560cf0dd6023935c22eb428
Astro <astro@spaceboyz.net>
2016-03-12 23:10:07 +0100

    serve app from all other paths

--- server/src/server.js
+++ server/src/server.js
@@ -6,7 +6,13 @@ var CONF = require('../config.js')
 
 app.use(require('compression')())
 app.use('/api', require('./api')(CONF))
-app.use(express.static(`${__dirname}/../../public`))
+var publicStatic = express.static(`${__dirname}/../../public`)
+app.use(publicStatic)
+app.use((req, res, next) => {
+    // Redirect all remaining paths to the single-page app
+    req.url = "/index.html"
+    publicStatic(req, res, next)
+})
 
 app.listen(CONF.http_port, function() {
     console.log(`Now listening on http://localhost:${CONF.http_port}/`)
